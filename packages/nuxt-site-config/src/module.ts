73b39eff14f3554afb0f0d82754162bf971ca162
SharpIce <VupRbl@163.com>
2026-05-16 22:26:19 +0800

    为 nutx-site-config 模块添加 url 字段的校验
    Signed-off-by: SharpIce <VupRbl@163.com>

--- packages/nuxt-site-config/src/module.ts
+++ packages/nuxt-site-config/src/module.ts
@@ -1,13 +1,35 @@
-import { defineNuxtModule } from '@nuxt/kit';
+import { defineNuxtModule, useLogger } from '@nuxt/kit';
 
 interface ModuleOptions {
+	/** 生存环境 URL */
 	url: string;
 }
+
+declare module '@nuxt/schema' {
+	interface NuxtOptions {
+		site: ModuleOptions;
+	}
+}
+
+const logger = useLogger('@anc/nuxt-site-config');
 
 export default defineNuxtModule<ModuleOptions>({
 	meta: {
 		name: '@anc/nuxt-site-config',
 		configKey: 'site',
+	},
+	setup(options, nuxt) {
+		const isDevelopment = nuxt.options.dev;
+		const localUrl = `http://${nuxt.options.devServer.host ?? '127.0.0.1'}:${nuxt.options.devServer.port}`;
+
+		if (!options.url) {
+			logger.warn('缺失 `url` 字段');
+			options.url = localUrl;
+		} else if (isDevelopment) {
+			options.url = localUrl;
+		}
+
+		nuxt.options.site = options;
 	},
 });
 
