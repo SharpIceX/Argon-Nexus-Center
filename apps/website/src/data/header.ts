2a8f75d081866ec5c5b0b74e15c6f4aafe415be0
SharpIce <VupRbl@163.com>
2026-05-16 21:07:52 +0800

    清理未完成的内容和更新依赖
    Signed-off-by: SharpIce <VupRbl@163.com>

--- apps/website/src/data/header.ts
+++ apps/website/src/data/header.ts
@@ -14,9 +14,9 @@ interface NavItemType {
 
 const navData: NavItemType[] = [
 	{ label: '首页', path: '/', icon: houseIcon },
-	{ label: '博客', path: '/blog', icon: bookIcon },
-	{ label: '图库', path: '/art', icon: clapperboardIcon },
-	{ label: '幻术奇象', path: '/ts', icon: atomIcon },
+	// { label: '博客', path: '/blog', icon: bookIcon },
+	// { label: '图库', path: '/art', icon: clapperboardIcon },
+	// { label: '幻术奇象', path: '/ts', icon: atomIcon },
 	{ label: '朋友们', path: '/friends', icon: heartIcon },
 ];
 
