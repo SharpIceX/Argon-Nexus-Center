b9cc31b369f1c3e61ad5e9cc1f00c656e9bac063
SharpIce <VupRbl@163.com>
2026-09-16 10:41:09 +0800

    chore: 一些杂项提交
    Signed-off-by: SharpIce <VupRbl@163.com>

--- apps/website/src/data/header.ts
+++ apps/website/src/data/header.ts
@@ -1,6 +1,7 @@
 import {
 	Atom as atomIcon,
 	Book as bookIcon,
+	Link as linkIcon,
 	Heart as heartIcon,
 	House as houseIcon,
 	Clapperboard as clapperboardIcon,
@@ -19,6 +20,7 @@ const navData: NavItemType[] = [
 	{ label: '艺术图库', path: '/gallery/', icon: clapperboardIcon },
 	// { label: '博客', path: '/blog/', icon: bookIcon },
 	// { label: '架空世界观', path: '/ts/', icon: atomIcon },
+	// { label: '联系我', path: '/connect/', icon: linkIcon },
 	{ label: '朋友们', path: '/friends/', icon: heartIcon },
 ];
 
